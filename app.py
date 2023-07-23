a9320b24c4119ac37613c3cb4c36fe6eafd84d95
terry0327 <67945641+terry0327@users.noreply.github.com>
2023-07-23 12:21:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def query(groupNode, user_id):
            print("group_data" + str(user_data))
            if isinstance(user_data, dict) and 'messages' in user_data:
                 print(str(user_data["messages"]))
-                report += "\n" + user_data["messages"]
+                report += "\n" + user_data[user_id]["messages"]
     else:
         report = "資料庫中並無資料，請先使用指令 !接龍 新增資料"
 
